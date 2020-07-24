88d799cbcf9b9f95300c64940c55ef3f960c90e9
georg3tom <georg3tom@gmail.com>
2020-07-24 19:56:14 +0530

    first commit

---
+++ alter/__init__.py
@@ -0,0 +1,75 @@
+import numpy as np
+import cv2
+
+class alter():
+    def __init__(self, imagePath):
+        self.imagePath = imagePath
+        self.img = cv2.imread(self.imagePath)
+
+    def rotate(self, angle, outfile):
+        image_center = tuple(np.array(self.img.shape[1::-1]) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+        result = cv2.warpAffine(self.img, rot_mat, self.img.shape[1::-1], flags=cv2.INTER_LINEAR)
+        cv2.imwrite(outfile,result)
+    
+    def scale(self, outfile, fx=0.5, fy=0.5):
+        result = cv2.resize(self.img, None, fx=fx, fy=fy, interpolation = cv2.INTER_CUBIC)
+        cv2.imwrite(outfile,result)
+
+    def affineTrans(self, outfile):
+        pass
+
+
+    def overlay(self, outfile, topImgPath='./hurr.png'):
+        topImg = cv2.imread(topImgPath, -1)
+        print(topImg.shape)
+        print(self.img.shape)
+        # topImg = cv2.resize(topImg, (self.img.shape[1], self.img.shape[0]))
+        # result = cv2.addWeighted(self.img, 0.5, topImg, 0.5, 0)
+        result = self.overlay_transparent(self.img.copy(), topImg, 0, 0)
+        cv2.imwrite(outfile, result)
+
+    def overlay_transparent(self, background, overlay, x, y):
+        """
+        used by overlay function
+        """
+        background_width = background.shape[1]
+        background_height = background.shape[0]
+
+        if x >= background_width or y >= background_height:
+            return background
+
+        h, w = overlay.shape[0], overlay.shape[1]
+
+        if x + w > background_width:
+            w = background_width - x
+            overlay = overlay[:, :w]
+
+        if y + h > background_height:
+            h = background_height - y
+            overlay = overlay[:h]
+
+        if overlay.shape[2] < 4:
+            overlay = np.concatenate(
+                [
+                    overlay,
+                    np.ones((overlay.shape[0], overlay.shape[1], 1), dtype = overlay.dtype) * 255
+                ],
+                axis = 2,
+            )
+
+        overlay_image = overlay[..., :3]
+        mask = overlay[..., 3:] / 255.0
+
+        background[y:y+h, x:x+w] = (1.0 - mask) * background[y:y+h, x:x+w] + mask * overlay_image
+
+        return background
+
+
+if __name__ == "__main__":
+    ins = alter('./outputname-1.png')
+    ins.rotate(25,'rotate.png')
+    ins.overlay('o.png')
+    ins.scale('s.png')
+
+
