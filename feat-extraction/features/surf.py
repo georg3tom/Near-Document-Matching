97d2d98786be181f18e26fe3cd45bc61d38e6b48
George Tom <georg3tom@gmail.com>
2020-08-05 19:15:04 +0530

    surf des

---
+++ feat-extraction/features/surf.py
@@ -0,0 +1,25 @@
+"""
+Returns the surf descriptors of an image
+"""
+
+import cv2
+import numpy as np
+
+
+def surf(img):
+    """
+    Returns the surf descriptors of image 
+    Expects an RGB image (3 channels)
+    """
+
+    surf = cv2.xfeatures2d.SURF_create()
+    kp, des = surf.detectAndCompute(img, None) 
+
+    return des
+
+
+if __name__ == "__main__":
+    img = cv2.imread("./test.png")
+    h = sift(img)
+    print(h.shape)
+
