d72b9ef8f9ac3fb6d014cd7bdef486f3e9a6a154
George Tom <georg3tom@gmail.com>
2020-08-08 09:07:27 +0530

    batch script for data gen

---
+++ data-gen/batch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A research
+#SBATCH -n 10
+#SBATCH -p long
+#SBATCH --gres=gpu:1
+#SBATCH --time=8:00:00
+
+pdfPath="./data/pdf/"
+imgPath="./data/img/"
+for i in $(ls $pdfPath);
+do
+    name=$(echo $i |sed 's/\./\n/g'  | head -n1)
+    pdftoppm $pdfPath$i $imgPath$name -png
+    echo $pdfPath$i
+done
+
+python3 data-gen.py
