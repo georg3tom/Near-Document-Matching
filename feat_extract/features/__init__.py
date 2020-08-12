cb1e745699789e50c8a3c9691ffde67ba1033af4
yoogottamk <yoogottamk@outlook.com>
2020-08-12 15:10:35 +0530

    update feature list

--- feat_extract/features/__init__.py
+++ feat_extract/features/__init__.py
@@ -7,6 +7,7 @@ from .edge import edge
 from .histogram import histogram
 from .hog import hog
 from .sift import sift
+from .stats import stats
 from .surf import surf
 
 features = [
@@ -18,5 +19,6 @@ experimental_features = [
     edge,
     hog,  # hog returns HUGE number of features, have to use it wisely
     sift,
+    stats,
     surf,
 ]
