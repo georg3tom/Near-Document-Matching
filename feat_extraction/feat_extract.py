c6fe16c4a5311a03471edcb7174eab874225b279
yoogottamk <yoogottamk@outlook.com>
2020-08-12 14:17:27 +0530

    added a preprocessing step

--- feat_extraction/feat_extract.py
+++ feat_extraction/feat_extract.py
@@ -2,39 +2,82 @@
 gets an image, returns all extracted features
 """
 
+import cv2
 import numpy as np
 from .features import features
 
 
-def getBlocks(img, num):
-    x = img.shape[0] // num
-    y = img.shape[1] // num
-    blocks = []
+class FeatureExtractor:
+    """
+    Given an image as input, it does a little preprocessing to make
+    it compatible.
 
-    for i in range(0, num - 1):
-        for j in range(0, num - 1):
-            blocks.append(img[i * x : (i + 1) * x, j * y : (j + 1) * y, :])
+    It can calculate features after breaking the image into segments
+    """
 
-        blocks.append(img[i * x : (i + 1) * x, (num - 1) * y :, :])
+    def __init__(self, img, segments=2):
+        self.img = img
+        self.segments = segments
 
-    for j in range(0, num - 1):
-        blocks.append(img[(num - 1) * x :, j * y : (j + 1) * y, :])
+        self.preprocess()
+        self.features = []
 
-    blocks.append(img[(num - 1) * x :, (num - 1) * y :, :])
+    def preprocess(self):
+        """
+        Make all images look mostly the same
+        """
+        self.img = cv2.resize(self.img, (900, 900), interpolation=cv2.INTER_AREA)
 
-    return blocks
+        if len(self.img.shape) == 2:
+            # got a grayscale image
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_GRAY2BGR)
+        else:
+            # image is "3D", might have an extra channel
+            n_channels = self.img.shape[-1]
 
+            if n_channels < 3:
+                # if shape is (x, y, 1) or something like this
+                self.img = np.dstack([self.img * (3 - n_channels)])
+            else:
+                # if it has a transparency channel as well
+                self.img = self.img[:, :, :3]
 
-def FeatureExtractor(img, num=2):
-    blocks = getBlocks(img, num)
-    feature_vector = np.zeros((0))
+    def break_blocks(self):
+        """
+        Breaks an image into the specified number of segments
+        """
 
-    for block in blocks:
-        for feature in features:
-            feature_vector = np.hstack([feature_vector, feature(block).ravel()])
+        # a turn-off switch of sorts
+        if self.segments <= 1:
+            return [self.img]
 
-    return feature_vector
+        x = self.img.shape[0] // self.segments
+        y = self.img.shape[1] // self.segments
 
+        blocks = []
 
-# class FeatureExtractor:
-#     pass
+        for i in range(0, self.segments - 1):
+            for j in range(0, self.segments - 1):
+                blocks.append(self.img[i * x : (i + 1) * x, j * y : (j + 1) * y, :])
+
+            blocks.append(self.img[i * x : (i + 1) * x, (self.segments - 1) * y :, :])
+
+        for j in range(0, self.segments - 1):
+            blocks.append(self.img[(self.segments - 1) * x :, j * y : (j + 1) * y, :])
+
+        blocks.append(self.img[(self.segments - 1) * x :, (self.segments - 1) * y :, :])
+
+        return blocks
+
+    def get_features(self):
+        """
+        Extracts all the features
+        """
+        blocks = self.break_blocks()
+        feature_vector = np.zeros((0))
+
+        for block in blocks:
+            for feature in features:
+                feature_vector = np.hstack([feature_vector, feature(block).ravel()])
+
+        return feature_vector
--- run.py
+++ run.py
@@ -3,25 +3,24 @@ import os
 import cv2
 import numpy as np
 
-from neigh_search import Lsh
+from neigh_search import LSH
 from feat_extraction import FeatureExtractor
 
-if __name__ == "__main__":
-    labels = []
-    vectors = []
+labels = []
+vectors = []
 
-    for image in os.listdir("./data"):
-        filename = "./data/" + image
-        img = cv2.imread(filename)
-        labels.append(image.split(".")[0])
-        vectors.append(np.resize(FeatureExtractor(img), 100))
+for image in os.listdir("./data"):
+    filename = "./data/" + image
+    img = cv2.imread(filename)
+    labels.append(image.split(".")[0])
+    vectors.append(np.resize(FeatureExtractor(img).get_features(), 100))
 
-    labels = np.array(labels)
-    vectors = np.array(vectors)
-    print(vectors.shape)
+labels = np.array(labels)
+vectors = np.array(vectors)
+print(vectors.shape)
 
-    lsh = Lsh(vectors, labels)
-    lsh.build()
+lsh = LSH(vectors, labels)
+lsh.build()
 
-    knns = lsh.query(vectors)
-    print(knns)
+knns = lsh.query(vectors)
+print(knns)
