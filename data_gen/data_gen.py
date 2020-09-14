e0934fae65ed886c792444da04bb228e18e3b502
georg3tom <georg3tom@gmail.com>
2020-09-14 22:00:13 +0530

    data gen changes

--- data_gen/data_gen.py
+++ data_gen/data_gen.py
@@ -16,16 +16,10 @@ if __name__ == "__main__":
 
     for f in files:
         altr = Alter(join(src, f))
-        altr.rotate(randint(5, 25)).write(directory=out)
-        altr = Alter(join(src, f))
-        altr.overlay().write(directory=out)
-        altr = Alter(join(src, f))
-        altr.scale().write(directory=out)
-        altr = Alter(join(src, f))
-        altr.affine_trans().write(directory=out)
+        altr.salt().write(directory=out)
 
         altr = Alter(join(src, f))
-        altr.rotate(randint(5, 25))
-        altr.scale().write(directory=out)
-        altr.overlay().write(directory=out)
-        altr.affine_trans().write(directory=out)
+        altr.pepper().write(directory=out)
+
+        altr = Alter(join(src, f))
+        altr.saltAndPepper().write(directory=out)
