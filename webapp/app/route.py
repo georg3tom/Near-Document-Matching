fd015ac75fcd2d23cc7ebadb25f44ef340226766
yoogottamk <yoogottamk@outlook.com>
2020-09-15 14:42:48 +0530

    improved webapp

--- webapp/app/route.py
+++ webapp/app/route.py
@@ -1,56 +1,79 @@
-from flask import render_template, url_for, request, jsonify
-from app import app
-
 import os
+from os.path import realpath, dirname
+import sys
 
 import cv2
+import faiss
+from flask import render_template, request, jsonify
 import numpy as np
+from app import app
 
-from app.neigh_search import LSH, L2
-from app.feat_extract import FeatureExtractor
+sys.path.insert(
+    0, realpath(os.path.join(dirname(realpath(__file__)), "../../")),
+)
 
-@app.route('/')
+from neigh_search import LSH
+from feat_extract import FeatureExtractor
+
+# either use absolute path OR make sure that
+# they are relative to `app`
+labels_path = "./labels"
+index_path = "./index"
+
+WEBAPP_CACHE = {}
+WEBAPP_LABELS = np.fromfile(labels_path, dtype="<U36")
+WEBAPP_INDEX = faiss.read_index(index_path)
+
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    """
+    Just for testing
+    """
+    return "Hello, World!"
 
-@app.route('/query')
+
+@app.route("/query")
 def query():
-    return render_template('main.html')
+    """
+    main page
+    """
+    return render_template("main.html")
 
-@app.route('/search', methods=['POST'])
+
+@app.route("/search", methods=["POST"])
 def search():
-    fname = request.form['filename']
-    try:
-        labels = []
-        vectors = []
-        query = []
-        imgPath = './app/static/image/'
-        images = os.listdir(imgPath)
-        if fname not in images:
-            return jsonify({'images': []})
-        total = len(images)
-        for i, image in enumerate(images):
-            filename = imgPath + image
-            img = cv2.imread(filename)
-            labels.append(image)
-            feat = FeatureExtractor(img, window_size={"h": 25, "w": 25}, window_stride={"h": 25, "w": 25}).get_features()
-            vectors.append(feat)
-            if image == fname:
-                query.append(feat)
+    """
+    Hashes the image and searchs for nearest neighbors
+    """
+    fname = request.form["filename"]
+    filepath = f"app/static/image/{fname}"
+    if not os.path.isfile(filepath):
+        print("Error, couldn't locate file")
+        return jsonify(images={})
 
-        labels = np.array(labels)
-        vectors = np.array(vectors)
-        query = np.array(query)
+    img = cv2.imread(filepath)
+    if img is None:
+        print("Error, couldn't open file")
+        return jsonify(images={})
 
-        lsh = LSH(vectors, labels)
-        lsh.build()
-        dist, knns = lsh.query(query)
+    print(f"Opened file. Dimen: {img.shape}")
 
-        # l2 = L2(vectors, labels)
-        # l2.build()
-        # dist, knns = l2.query(vectors)
-        return jsonify({'images': list(knns[0])})
+    # check cache
+    print("Looking for image in cache")
+    if fname in WEBAPP_CACHE:
+        print("Found!")
+        return WEBAPP_CACHE[fname]
 
-    except:
-        print('error')
-        return jsonify({'images': []})
+    print("Couldn't find image in cache")
+    print("Calculating features...")
+    features = np.array([FeatureExtractor(img).get_features()], dtype=np.float32)
+    print(f"Done!\nFeature shape: {features.shape}")
+
+    print("Querying...")
+    _, indices = WEBAPP_INDEX.search(features, 1)
+    print("Done!")
+    ret = jsonify(images=WEBAPP_LABELS[np.array(indices)])
+    WEBAPP_CACHE[fname] = ret
+
+    return ret
