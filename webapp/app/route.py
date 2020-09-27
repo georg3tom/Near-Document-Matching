8167c210e38e4ac5730bf165a792217225586d2d
georg3tom <georg3tom@gmail.com>
2020-09-27 20:00:26 +0530

    added file upload

--- webapp/app/route.py
+++ webapp/app/route.py
@@ -1,6 +1,8 @@
 import os
 from os.path import realpath, dirname
 import sys
+import io
+from PIL import Image
 
 import cv2
 import faiss
@@ -18,8 +20,8 @@ from feat_extract import FeatureExtractor
 
 # either use absolute path OR make sure that
 # they are relative to `app`
-labels_path = "./labels.npy"
-index_path = "./index"
+labels_path = "../labels.npy"
+index_path = "../index"
 
 WEBAPP_CACHE = {}
 WEBAPP_LABELS = np.load(labels_path)
@@ -76,5 +78,33 @@ def search():
     print("Done!")
     ret = jsonify(images=list(WEBAPP_LABELS[np.array(indices)][0]))
     WEBAPP_CACHE[fname] = ret
+
+    return ret
+
+@app.route("/searchUpload", methods=["POST"])
+def searchUpload():
+    """
+    Hashes the image and searchs for nearest neighbors
+    """
+    print(request.files["file-0"])
+    f = request.files["file-0"]
+
+    img = Image.open(io.BytesIO(f.stream.read()))
+    img = cv2.cvtColor(np.float32(img), cv2.COLOR_RGB2BGR)
+
+    if img is None:
+        print("Error, couldn't open file")
+        return jsonify(images={})
+
+    print(f"Received file. Dimen: {img.shape}")
+
+    print("Calculating features...")
+    features = np.array([FeatureExtractor(img).get_features()], dtype=np.float32)
+    print(f"Done!\nFeature shape: {features.shape}")
+
+    print("Querying...")
+    _, indices = WEBAPP_INDEX.search(features, 10)
+    print("Done!")
+    ret = jsonify(images=list(WEBAPP_LABELS[np.array(indices)][0]))
 
     return ret
